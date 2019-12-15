a472ed1ac358c1a4849975b4308d30c47b857c57
Harsh <48208148+Kaustav112@users.noreply.github.com>
2019-12-15 22:17:46 +0530

    Update mqtt.py

--- mqtt.py
+++ mqtt.py
@@ -4,12 +4,12 @@ import time
 def on_msg(client,username,data):
   print("Msg:",str(msg.payload.decode("utf-8")))
   
- client = mqtt.Client("P1")
- client.on_message = on_msg
+client = mqtt.Client("P1")
+client.on_message = on_msg
  
- client.connect("192.168.12.1")         ##IP to be changed
- client.loop_start()
- client.subscribe("a/b")
- client.publish("a/b","Harsh")          ##Name to be changed
- time.sleep(10)
- client.loop_stop()
+client.connect("192.168.12.1")         ##IP to be changed
+client.loop_start()
+client.subscribe("a/b")
+client.publish("a/b","Harsh")          ##Name to be changed
+time.sleep(10)
+client.loop_stop()
