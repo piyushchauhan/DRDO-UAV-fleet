3075b4a3d2969230e17aafb5f0f24ba04aaa1c4e
Harsh <48208148+Kaustav112@users.noreply.github.com>
2019-12-16 03:40:45 +0530

    Update mqtt.py

--- mqtt.py
+++ mqtt.py
@@ -13,3 +13,12 @@ client.subscribe("a/b")
 client.publish("a/b","Harsh")          ##Name to be changed
 time.sleep(10)
 client.loop_stop()
+
+
+###Standalone Publisher Script
+import paho.mqtt.publish as publish
+
+mqtt_server = "192.168.43.23"
+mqtt_path = "test_channel"
+
+publish.single(mqtt_path,"Text Message",hostname = mqtt_server)
