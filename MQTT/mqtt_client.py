1f12bd597bdf6065564bde87cc6191d53b105740
Harsh <48208148+Kaustav112@users.noreply.github.com>
2019-12-16 15:05:38 +0530

    Create mqtt_client.py

---
+++ MQTT/mqtt_client.py
@@ -0,0 +1,29 @@
+import paho.mqtt.client as mqtt
+
+import time
+
+
+
+def on_msg(client,username,data):
+
+    print("Msg:",str(msg.payload.decode("utf-8")))
+
+  
+
+client = mqtt.Client("P1")
+
+client.on_message = on_msg
+
+ 
+
+client.connect("192.168.12.1")         ##IP to be changed
+
+client.loop_start()
+
+client.subscribe("a/b")
+
+client.publish("a/b","Harsh")          ##Name to be changed
+
+time.sleep(10)
+
+client.loop_stop()
